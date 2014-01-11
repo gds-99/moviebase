21d2e6e479618d6b25a36f90a678cb61630e8c8f
greg silagi <gsilagi@vmware.com>
2014-01-10 16:33:16 -0800

    final changes

--- src/main/java/com/vmware/moviebase/MovieService.java
+++ src/main/java/com/vmware/moviebase/MovieService.java
@@ -1,47 +1,40 @@
 package com.vmware.moviebase;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MovieService {
-	private List<Movie> movies = new ArrayList<Movie>();
-	private int count = 0;
-	
-		
-	public  MovieService(String title) {
-		
+	private Set<Movie> movies = new HashSet<Movie>();
+
+	public MovieService(String title) {
+
 	}
 
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return  count;
+		return movies.size();
 	}
 
-	public void addMovie(Movie movie) {
-				count++;
-				movies.add(movie);
-		
+	public void addMovie(Movie movie)throws MovieException {
+if (movies.contain(movie.getName()))
+			throw new MovieException("Movie already exist");
+		movies.add(movie);
+
 	}
 
-	public void removeByName(String name)throws MovieException {
-		// TODO Auto-generated method stub
-		  if (count == 0) 
-			  	throw new MovieException( "There are no movies in the service");
-		  for (Movie m : movies) {
-			  if (m.getName() .equals (name)){
-				  	movies.remove(m) ;
-				  	count--;
-				  	return;
-			  }
-		  }
-		  	
-		  throw new MovieException("that movie does not exist in the service");
-		  
-		  }
-		  
+	public void removeByName(String name) throws MovieException {
+
+		if (getCount() == 0)
+			throw new MovieException("There are no movies in the service");
+		for (Movie m : movies) {
+			if (m.getName().equals(name)) {
+				movies.remove(m);
+				return;
+			}
+		}
+
+		throw new MovieException("that movie does not exist in the service");
+
 	}
 
-	
-	
-	
- 
+}
